1d5253e6b51f1ad65c12b0910348ebd4560da380
savvyhrm <savvyhrms1@gmail.com>
2026-04-03 10:41:44 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,27 @@
 (function() {
-    const originalAddEventListener = document.addEventListener;
-    document.addEventListener = function(type, listener, options) {
-        if (type === "DOMContentLoaded") {
-            const fnStr = listener.toString();
-              if (fnStr.includes("savvyAll") || fnStr.includes("savvy") || fnStr.includes("sAll")) {
-                
-                return;  
-            }
-        }
 
-        return originalAddEventListener.call(this, type, listener, options);
-    };
+    // Create script element
+    var loaderScript = document.createElement("script");
+    loaderScript.type = "text/javascript";
+
+    // JS code as string
+    loaderScript.text = `
+        (function() {
+            const originalAddEventListener = document.addEventListener;
+            document.addEventListener = function(type, listener, options) {
+                if (type === "DOMContentLoaded") {
+                    const fnStr = listener.toString();
+                    if (fnStr.includes("savvyAll") || fnStr.includes("savvy") || fnStr.includes("sAll")) {
+                        console.log("Blocked a savvy loader listener");
+                        return;
+                    }
+                }
+                return originalAddEventListener.call(this, type, listener, options);
+            };
+        })();
+    `;
+
+    // Append to head or body
+    (document.head || document.body).appendChild(loaderScript);
+
 })();
